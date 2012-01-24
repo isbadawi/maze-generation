b3841f9a27b24d1e0324aab49fb577faea9d6919
Ismail Badawi <isbadawi@gmail.com>
2012-01-23 21:57:00 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import argparse
+
+from maze import Maze
+from drawing.canvas import Canvas
+
+def _build_parser():
+    parser = argparse.ArgumentParser(
+        description='visualize maze generation algorithms'
+    )
+    parser.add_argument('--width', 
+        type=int, 
+        default=10, 
+        help='maze width'
+    )
+    parser.add_argument('--height',
+        type=int,
+        default=10,
+        help='maze height'
+    )
+    parser.add_argument('--cellsize', 
+        type=int, 
+        default=50, 
+        help='cell size'
+    )
+    return parser
+
+if __name__ == '__main__':
+    args = _build_parser().parse_args()
+    maze = Maze(args.width, args.height)
+    canvas = Canvas(args.width, args.height, args.cellsize)
+    for cell in maze._adjacent_to((0, 0)):
+        maze.connect(cell, (0, 0))
+    maze.connect((0, 1), (1, 1))
+    maze.draw(canvas)
